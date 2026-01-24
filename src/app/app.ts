f34fef6ed847eb2632d0b7e19d5cb33ab2fb11cf
Olya Vo <olga.volkova@email.cz>
2026-01-24 21:40:30 +0100

    app with OnPush

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'app-root'
+  }
 })
 export class App {
-  protected readonly title = signal('how-to');
 }
