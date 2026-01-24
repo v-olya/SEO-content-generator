a27265a8bab731ee3a96396042b97753ebec8e95
Olya Vo <olga.volkova@email.cz>
2026-01-24 22:19:14 +0100

    remove toolbar from imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToolbarModule } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToolbarModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
