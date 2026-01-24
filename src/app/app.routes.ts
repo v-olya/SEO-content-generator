8e082ba55cbabd180f5c72adabeb1c57aa3c2304
Olya Vo <olga.volkova@email.cz>
2026-01-24 22:07:37 +0100

    home and cluster-detail routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		loadComponent: () => import('./pages/home.page').then((m) => m.HomePage)
+	},
+	{
+		path: ':slug',
+		loadComponent: () => import('./pages/cluster-page').then((m) => m.ClusterPage)
+	},
+	{
+		path: '**',
+		redirectTo: ''
+	}
+];
