0c5dded897a2ebca8fd9c7837f4adbe01f674482
Olya Vo <olga.volkova@email.cz>
2026-01-25 01:41:45 +0100

    fix the path problem with eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['tsconfig.app.json', 'tsconfig.spec.json', 'backend/tsconfig.json'],
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+      },
+      excludedFiles: ['backend/src/cluster/constants.ts'],
+      plugins: ['@typescript-eslint', '@angular-eslint'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+      ],
+    },
+    {
+      files: ['*.html'],
+      parser: '@angular-eslint/template-parser',
+      extends: ['plugin:@angular-eslint/template/recommended'],
+    },
+  ],
+};
