bfa29deab09e3019ff01b5f6d257e4f680f2cebe
Olya Vo <olga.volkova@email.cz>
2026-01-25 16:07:45 +0100

    add generation-related methods

--- src/app/services/cluster-api.service.ts
+++ src/app/services/cluster-api.service.ts
@@ -1,6 +1,11 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ClusterDetailResponse, ClusterResponse } from '../types';
+import {
+  ArticleStatusResponse,
+  ClusterDetailResponse,
+  ClusterResponse,
+  StartArticleResponse,
+} from '../types';
 import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
@@ -14,5 +19,16 @@ export class ClusterApiService {
 
   getClusterDetail(jobId: string, slug: string) {
     return this.http.get<ClusterDetailResponse>(`${this.baseUrl}/cluster/${jobId}/${slug}`);
+  }
+
+  startArticleGeneration(jobId: string, slug: string) {
+    return this.http.post<StartArticleResponse>(`${this.baseUrl}/article/generate`, {
+      jobId,
+      slug,
+    });
+  }
+
+  getArticleStatus(articleId: string) {
+    return this.http.get<ArticleStatusResponse>(`${this.baseUrl}/article/status/${articleId}`);
   }
 }
