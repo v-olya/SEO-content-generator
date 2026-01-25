22623bdabd25ee36bbf2eca97038edafcae4673b
Olya Vo <olga.volkova@email.cz>
2026-01-25 03:28:23 +0100

    use helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
   const corsOrigin = configService.get<string>('CORS_ORIGIN', 'http://localhost:4200');
 
+  app.use(helmet());
   app.enableCors({
     origin: corsOrigin.split(','),
     credentials: false,
